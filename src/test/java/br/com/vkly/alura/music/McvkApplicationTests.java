51e744f3ae73a0344ec82e5ff9cbcf108e88077b
vkaczmarzykly <vkaczmarzykly@gmail.com>
2024-09-16 20:30:39 -0300

    FIRST COMMIT

---
+++ src/test/java/br/com/vkly/alura/music/McvkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vkly.alura.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McvkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
